e6d47840bb2b9146648ea7a2ba2416121b006813
Udodirim <Udodirimwisdom@gmail.com>
2024-06-13 14:51:15 +0100

    security

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,10 +14,14 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1) GLOBAL MIDDLEWARES
+// Set security HTTP header
+app.use(helmet());
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limiting the request from one IP per hour
 const limiter = rateLimit({
   max: 500, // TODO: Adjust the value to meet the requirement of the specific application
   timeWindowMs: 60 * 60 * 1000,
@@ -21,7 +29,50 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser
+app.use(express.json({ limit: '10kb' })); // TODO: Adjust the value to meet the requirement of the specific application
+
+// Data sanitization against NoSQL injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS attacks
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+      //,
+      // 'priceDiscount',
+      // 'summary',
+      // 'description',
+      // 'imageCover',
+      // 'images',
+      // 'createdAt',
+      // 'updatedAt',
+      // 'startDates',
+      // 'startLocation',
+      // 'startLocation.type',
+      // 'startLocation.coordinates',
+      // 'startLocation.address',
+      // 'startLocation.description',
+      // 'locations',
+      // 'locations.type',
+      // 'locations.coordinates',
+      // 'locations.address',
+      // 'locations.description',
+      // 'locations.day'
+    ]
+  })
+);
+
+// Serving static file
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
