1ede39fde65c3694c565e1555c60eaa75066bdcd
Udodirim <Udodirimwisdom@gmail.com>
2024-06-16 20:36:26 +0100

    Review nested route functionality implemented

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
 const tourController = require('./../controllers/tourController');
-const authController = require('../controllers/authController');
+const authController = require('./../controllers/authController');
+const reviewRoute = require('./../routes/reviewRoutes');
 
 const router = express.Router();
 
 // router.param( 'id', tourController.checkID );
+router.use('/:tourId/reviews', reviewRoute);
 
 // 5 BEST AND CHEAPEST
 router
